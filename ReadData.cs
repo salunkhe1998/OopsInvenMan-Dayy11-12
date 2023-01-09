a10cd9e268937d9e335c448ba421188a669757ff
Ashwini <salunkheashwini1998@gmail.com>
2023-01-09 16:28:04 +0530

    this is file

---
+++ ReadData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryManagementProgram
+{
+
+    public class ReadData
+    {
+        public InventoryDetails Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
